1b0f0c3f84caff11f1d09b2fcda997a9c598562c
Bong <mjbond_bong2@yahoo.com>
2017-03-09 10:39:16 +0800

    add recipe create page/ recipe routes/ tested ok

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,41 @@
 class RecipesController < ApplicationController
+	before_action :require_login, except: [:index, :show]
+
+	def index
+	end
+
+	def show
+		@recipe =  Recipe.all
+	end
+
+	def new
+		@recipe = Recipe.new
+
+	end
+
+	def create
+		@recipe = current_user.recipes.new(recipe_params)
+		
+		if @recipe.save
+		    flash[:success] = "Successfully added a new listing!"
+		    redirect_to root_path
+		else
+		    flash[:danger] = @recipe.errors.full_messages.join(', ')
+		    redirect_to new_recipe_path
+		end
+
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+	def recipe_params
+		params.require(:recipe).permit(:title, :cook_time, :nutrition, :content)
+	end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170308034939) do
+ActiveRecord::Schema.define(version: 20170309023704) do
 
   create_table "recipes", force: :cascade do |t|
     t.string   "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170308034939) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "user_id"
   end
 
   create_table "users", force: :cascade do |t|
